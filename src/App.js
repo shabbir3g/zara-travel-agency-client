00bfcb069a07f4adf3a358ccacadc2fb91953f89
shabbir3g <mostafizshabbir@gmail.com>
2022-01-02 12:39:52 +0600

    added something

--- src/App.js
+++ src/App.js
@@ -1,38 +1,24 @@
 // import './App.css';
-<<<<<<< HEAD
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes, Route } from "react-router-dom";
 import Banner from "./components/Home/Banner/Banner.js";
+import Service from "./components/Home/Service/Service.js";
 import Login from "./components/Home/Login/Login/Ligin.js";
 import Register from "./components/Home/Login/Register/Register.js";
 import AuthProvider from "./contexts/AuthProvider/AuthProvider.js";
-=======
-import { Routes, Route} from "react-router-dom";
-import Service from "./components/Home/Service/Service";
-// import Banner from "./components/Home/Banner/Banner.js";
-
-
->>>>>>> 3a6117f0bb444b6c7af0fa450cde3c6ed68f3dd5
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <AuthProvider>
         <Routes>
           <Route path="/" element={<Banner />} />
           <Route path="/home" element={<Banner />} />
+          <Route path="/service" element={<Service />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
       </AuthProvider>
-=======
-          <Routes>
-             <Route path="/" element={<Service />} />
-            
-          </Routes>
-         
->>>>>>> 3a6117f0bb444b6c7af0fa450cde3c6ed68f3dd5
     </div>
   );
 }
