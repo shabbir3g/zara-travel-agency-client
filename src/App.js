8da277a50beec448ee08977b3841ad94ee250c5e
Prantosh <bepariprantosh360@gmail.com>
2022-01-02 13:49:49 +0600

    change and update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 // import './App.css';
-<<<<<<< HEAD
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes, Route } from "react-router-dom";
 import Banner from "./components/Home/Banner/Banner.js";
 import Login from "./components/Home/Login/Login/Ligin.js";
 import Register from "./components/Home/Login/Register/Register.js";
 import AuthProvider from "./contexts/AuthProvider/AuthProvider.js";
-=======
-import { Routes, Route} from "react-router-dom";
-import Service from "./components/Home/Service/Service";
-// import Banner from "./components/Home/Banner/Banner.js";
-
-
->>>>>>> 3a6117f0bb444b6c7af0fa450cde3c6ed68f3dd5
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <AuthProvider>
         <Routes>
           <Route path="/" element={<Banner />} />
@@ -26,13 +17,6 @@ function App() {
           <Route path="/register" element={<Register />} />
         </Routes>
       </AuthProvider>
-=======
-          <Routes>
-             <Route path="/" element={<Service />} />
-            
-          </Routes>
-         
->>>>>>> 3a6117f0bb444b6c7af0fa450cde3c6ed68f3dd5
     </div>
   );
 }
